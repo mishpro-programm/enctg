cb02e3f417237fd940b97a74e12737fc596006da
mishpro-programm <root@venusbiser.ru>
2023-07-03 04:52:39 +0000

    verbose

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 import os
-os.system("pip install -r requirements.txt")
+print("\e[0;35mУстановка зависимостей...")
+os.popen("pip install -r requirements.txt")
+print("\e[0;35mЗависимости установлены. Запуск скрипта.\nСейчас вам потребуется ввести свой номер и код.\e[0;0m")
 os.system("cd dist; python3 main.py")
