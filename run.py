d7796cb193b4c7c279dd946a626a76bf983e64c3
mishpro-programm <root@venusbiser.ru>
2023-07-03 05:26:24 +0000

    pyinstaller

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import os
 print("\033[33mУстановка зависимостей...")
 os.popen("pip install -r requirements.txt")
 print("\033[33mЗависимости установлены. Запуск скрипта.\nСейчас вам потребуется ввести свой номер и код.\033[0m")
-os.system("cd dist; python3 main.py")
+os.system("cd dist/main; ./main")
